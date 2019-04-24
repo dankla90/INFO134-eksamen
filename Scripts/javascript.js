960ecdfb4e3a6d614b8f8de3a41ca20a7873beec
Daniel Klausen <danielklausen90@gmail.com>
2019-04-24 10:56:58 +0200

    mindre endringer

---
+++ Scripts/javascript.js
@@ -0,0 +1,15 @@
+console.log("test if test");
+
+function startProgram() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "http://wildboy.uib.no/~tpe056/folk/104857.json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log("Type", xhr.getResponseHeader("Content-Type"));
+            console.log("Text", xhr.responseText);
+        }
+    };
+    xhr.send();
+}
+
+window.onload = startProgram;
