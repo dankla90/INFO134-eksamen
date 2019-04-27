52a8c9f79209c9bbcd00b6429a196ba4b7ff67f5
dankla90 <43035752+dankla90@users.noreply.github.com>
2019-04-27 15:25:32 +0200

    ryddet litt

--- Scripts/javascript.js
+++ Scripts/javascript.js
@@ -1,3 +1,13 @@
+
+
+
+// lager objecter
+var befolkning;
+var sysselsatte;
+var utdannede;
+
+
+
 //testing getting all the JSON documents with the same function at the start
 
 //creates an array of the identifications for the JSON documents
@@ -6,6 +16,7 @@ var indexUrl = ["104857.json", "100145.json", "85432.json"];
      
     var url = "http://wildboy.uib.no/~tpe056/folk/" + indexUrl[i];
       
+        //kjører alle requestene individuelt og ikke sequensially 
         let request = new XMLHttpRequest(); 
         request.onreadystatechange = function() {
             if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
@@ -20,11 +31,23 @@ var indexUrl = ["104857.json", "100145.json", "85432.json"];
 }
 
 
+//lagrer Parsed JSON data på det tilsvarende objectet
+function lagre_data(data){
+    if(data.datasett.opphav == "http://data.ssb.no/api/v0/dataset/85432?lang=no"){
+        utdannede = data;
+        console.log("works");
+    } else if(data.datasett.elementer == "Overhalla") {
+        befolkning = data;
+    } else {
+        sysselsatte = data;
+    };
+    console.log("works");
+    console.log(utdannede);
+}	
 
-// lager objecter
-var befolkning;
-var sysselsatte;
-var utdannede;
+
+
+
 
 //en test
 function test(){
@@ -35,18 +58,3 @@ function test(){
     console.log("test ferdig")
 };
 setTimeout(test, 2000);
-
-
-//lagrer Parsed JSON data på det tilsvarende objectet
-function lagre_data(data){
-    if(data.datasett.opphav == "http://data.ssb.no/api/v0/dataset/85432?lang=no"){
-        utdannede = data;
-        console.log("works");
-    } else if(data.datasett.opphav == "http://data.ssb.no/api/v0/dataset/104857?lang=no") {
-        befolkning = data;
-    } else {
-        sysselsatte = data;
-    };
-    console.log("works");
-    console.log(utdannede);
-}	
