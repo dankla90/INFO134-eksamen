02e438f4c5e9f7beec0c387212163c9d148ad0ae
scomon <shabaz26@gmail.com>
2019-05-08 00:09:55 +0200

    Update javascript.js
    Lagt til funksjonene sisteSysselsetting(), sisteHoyereUtdanning(), oversiktKommune(kom_nr), getDetaljer().

--- Scripts/javascript.js
+++ Scripts/javascript.js
@@ -9,67 +9,62 @@ var utdannede;
 
 
 /*Konstruktøren virker som grensesnitt mot hvert datasett*/
-	function Folk(url){
-		this.url = url;
-		this.getInfo = function(){getInfo()};
-		this.getNames = function(){getNames()};
-		this.getIDs = function(){getIDs()};
-    //this.load = function() {hentJSON(this.url,lagre_data)};
-	}
-
-
-
+function Folk(url){
+	this.url = url;
+	this.getInfo = function(){getInfo()};
+	this.getNames = function(){getNames()};
+	this.getIDs = function(){getIDs()};
+	//this.load = function() {hentJSON(this.url,lagre_data)};
+}
 
 //testing getting all the JSON documents with the same function at the start
-
 //creates an array of the identifications for the JSON documents
 var indexUrl = ["104857.json", "100145.json", "85432.json"];
 for (var i = 0; i < indexUrl.length; i++) {
 
-  var url = "http://wildboy.uib.no/~tpe056/folk/" + indexUrl[i];
+	var url = "http://wildboy.uib.no/~tpe056/folk/" + indexUrl[i];
 
 
-  //kjører alle requestene individuelt og ikke sequensially
-  let request = new XMLHttpRequest();
-  request.onreadystatechange = function() {
-    if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
-      var content = this.responseText;
-      var data = JSON.parse(content);
-      console.log(data);
-      lagre_data(data);
-    }
-  }
-  request.open("GET", url);
-  request.send();
+	//kjører alle requestene individuelt og ikke sequensially
+	let request = new XMLHttpRequest();
+	request.onreadystatechange = function() {
+		if(request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+			var content = this.responseText;
+			var data = JSON.parse(content);
+			console.log(data);
+			lagre_data(data);
+		}
+	}
+	request.open("GET", url);
+	request.send();
 }
 
 
 //lagrer Parsed JSON data på det tilsvarende objectet
 function lagre_data(data){
-  if(data.datasett.opphav == "http://data.ssb.no/api/v0/dataset/85432?lang=no"){
-    utdannede = data;
-    console.log("works");
-  } else if(data.datasett.opphav == "http://data.ssb.no/api/v0/dataset/104857?lang=no") {
-    befolkning = data;
-  } else {
-    sysselsatte = data;
-  };
-  console.log("works");
-  console.log(utdannede);
+	if(data.datasett.opphav == "http://data.ssb.no/api/v0/dataset/85432?lang=no"){
+		utdannede = data;
+		console.log("works");
+	} else if(data.datasett.opphav == "http://data.ssb.no/api/v0/dataset/104857?lang=no") {
+		befolkning = data;
+	} else {
+		sysselsatte = data;
+	};
+	console.log("works");
+	console.log(utdannede);
 }
 
 
 
 //Funksjonen returnerer listen av alle kommunenummerene.
 function getIDs(){
-
-  //Oppretter tabell for visning av data
-  let kommune_nummer ="<table><tr><td><b>Kommunenr.</b></td></tr>";
-  for(var id in datasett.elementer){
-    kommune_nummer += "<tr><td>"+datasett.elementer[id].kommunenummer+"</td></tr>";
-  }
-    kommune_nummer += "</table>";
-  return kommune_nummer;
+	//Oppretter tabell for visning av data
+	let kommune_nummer ="<table><tr><td><b>Kommunenr.</b></td></tr>";
+	for(var id in datasett.elementer){
+		kommune_nummer += "<tr><td>"+datasett.elementer[id].kommunenummer+"</td></tr>";
+	}
+	kommune_nummer += "</table>";
+	return kommune_nummer;
 }
 
 
@@ -77,13 +72,13 @@ function getIDs(){
 
 //Funksjonen henter navn på kommuner i datasettet
 function getNames(){
-  //Oppretter tabell for visning av data
-  let names = "<table><tr><td><b>Kommunenavn</b></td></tr>";
-  for(name in datasett.elementer){
-    names+="<tr><td>" + name + "</td></tr>";//Legger elementer i tabell
-  }
-  names += "</table>";
-  return names;
+	//Oppretter tabell for visning av data
+	let names = "<table><tr><td><b>Kommunenavn</b></td></tr>";
+	for(name in datasett.elementer){
+		names+="<tr><td>" + name + "</td></tr>";//Legger elementer i tabell
+	}
+	names += "</table>";
+	return names;
 }
 
 
@@ -91,103 +86,191 @@ function getNames(){
 //Funksjonen returnerer samlet befolkningstall for kvinner og menn
 function getBefolkning(){
 
-  var tabell_samlet = [];
-  var tabell_kvinner = [];
-  var tabell_menn = [];
+	var tabell_samlet = [];
+	var tabell_kvinner = [];
+	var tabell_menn = [];
 
 
-  let innhold = "<table><b>Befolkning</b>";//tabell for visning av data
-  for(var indeks in datasett.elementer){
-    tabell_menn.push([datasett.elementer[indeks]["Menn"][2018]]);//Legger til tall for menn
-    tabell_kvinner.push([datasett.elementer[indeks]["Kvinner"][2018]]);//Legger til tall for kvinner
-  }
+	let innhold = "<table><b>Befolkning</b>";//tabell for visning av data
+	for(var indeks in datasett.elementer){
+		tabell_menn.push([datasett.elementer[indeks]["Menn"][2018]]);//Legger til tall for menn
+		tabell_kvinner.push([datasett.elementer[indeks]["Kvinner"][2018]]);//Legger til tall for kvinner
+	}
 
-  //Legger summen av antall kvinner og menn i en samlet tabell
-  for(var i=0; i<tabell_kvinner.length; i++){
-    tabell_samlet =  tabell_kvinner[i].shift() + tabell_menn[i].shift();//Summerer første indeks fra hver tabell
-    innhold +=  "<tr><td>"+tabell_samlet+"</td></tr>";//samlet befolkningstall 2018
-  }
-  innhold += "</table>";
+	//Legger summen av antall kvinner og menn i en samlet tabell
+	for(var i=0; i<tabell_kvinner.length; i++){
+		tabell_samlet =  tabell_kvinner[i].shift() + tabell_menn[i].shift();//Summerer første indeks fra hver tabell
+		innhold +=  "<tr><td>"+tabell_samlet+"</td></tr>";//samlet befolkningstall 2018
+	}
+	innhold += "</table>";
 
-  return innhold;
+	return innhold;
 }
 
 
 
-	//Funksjonen returnerer en oversikt over kommunenummer, kommunenavn og siste befolkningstall(2018)
-	function getOversikt(){
+//Funksjonen returnerer en oversikt over kommunenummer, kommunenavn og siste befolkningstall(2018)
+function getOversikt(){
 
-		let innhold = "<table style=width:50%><b>Siste måling av total befolkning - 2018</b><br><br>";
-			innhold+= "<tr><td>"+getIDs() + "</td><td>"+getNames() + "</td><td>"+ getBefolkning()+"</td></tr>"
+	let innhold = "<table style=width:50%><b>Siste måling av total befolkning - 2018</b><br><br>";
+	innhold+= "<tr><td>"+getIDs() + "</td><td>"+getNames() + "</td><td>"+ getBefolkning()+"</td></tr>"
 
-		    document.getElementById("oversikt").innerHTML = innhold;
+	document.getElementById("oversikt").innerHTML = innhold;
+}
+
+
+//Funksjonen henter informasjon for angitte kommunenummer.
+function getInfo(kommune_nr){
+
+	var innhold;
+	var kommune_nr = document.getElementById("kom_nr").value; //henter ut verdien fra inputfeltet
+
+	for(var indeks in datasett.elementer){
+		let kom_nummer = datasett.elementer[indeks].kommunenummer;
+
+		//Sammenligner kommunenr.
+		if(kommune_nr == kom_nummer){
+			//Henter informasjon fra kommune
+			innhold += indeks+"<tr><td> kommune</td></tr>"+"<tr><td>"+JSON.stringify(datasett.elementer[indeks])+"</td></tr>";
+		}
 	}
+	console.log(innhold);
+	return innhold;
+}
 
 
-  //Funksjonen henter informasjon for angitte kommunenummer.
-  function getInfo(kommune_nr){
 
-    var innhold;
-    var kommune_nr = document.getElementById("kom_nr").value; //henter ut verdien fra inputfeltet
+//Funksjonen henter siste tall for sysselsetting for begge kjønn.
+function sisteSysselsetting(){
+
+	let beggeKjonn;
+	var kom_nr = document.getElementById("kom_nr").value; //henter ut verdien fra inputfeltet
+
+	for(var indeks in datasett.elementer){
+		let kommune_nr = datasett.elementer[indeks].kommunenummer;
+		if(kom_nr == kommune_nr){
+			beggeKjonn = datasett.elementer[indeks]["Begge kjønn"][2018];
+		}
+	}
+	//document.getElementById("detaljer").innerHTML = beggeKjonn+"%";
+	return beggeKjonn+" %";
+}
 
 
-    for(var indeks in datasett.elementer){
-      let kom_nummer = datasett.elementer[indeks].kommunenummer;
 
-      //Sammenligner kommunenr.
-      if(kommune_nr == kom_nummer){
 
-        //Henter informasjon fra kommune
-        innhold += indeks+"<tr><td> kommune</td></tr>"+"<tr><td>"+JSON.stringify(datasett.elementer[indeks])+"</td></tr>";
-      }
-    }
-    console.log(innhold);
-    return innhold;
-  }
+//Funksjonen henter tall for høyre utdanning(lang)
+function sisteHoyereUtdanning(){
+
+	var kom_nr = document.getElementById("kom_nr").value; //henter ut verdien fra inputfeltet
+
+	let tall_kvinner;
+	let tall_menn;
+	let samlet_tall;
+
+	for(var indeks in datasett.elementer){
+		let kommune_nr = datasett.elementer[indeks].kommunenummer;
+		if(kom_nr == kommune_nr){
+			tall_menn = datasett.elementer[indeks]["04a"].Menn[2017];
+			tall_kvinner = datasett.elementer[indeks]["04a"].Kvinner[2017];
+			samlet_tall = tall_menn + tall_kvinner;
+		}
+	}
+	return samlet_tall;
+}
+
+
+
+//Funksjonen returnerer befolkningstall for angitt kommunenummer.
+function oversiktKommune(kom_nr){
+
+	let tallMenn;
+	let tallKvinner;
+	let innhold;
+
+	for(var indeks in datasett.elementer){
+		let kommune_nr = datasett.elementer[indeks].kommunenummer;
+
+		if(kom_nr == kommune_nr){
+
+			tallMenn = datasett.elementer[indeks]["Menn"][2018];
+			tallKvinner = datasett.elementer[indeks]["Kvinner"][2018];
+		}
+	}
+	innhold = tallMenn + tallKvinner;//Legger sammen tall for kvinner og menn
+	return innhold;
+}
+
+
+
+
+/*
+Oppgave 1.1:"detaljer"
+Funksjonen returnerer kommunens navn, kommunenummer, siste målte befolkning,
+siste målte statistikk for sysselsetting og høyere utdanning (antall og prosent) for angitt kommunenummer.
+Metoden er ikke helt ferdig!.
+*/
+function getDetaljer(){
+
+	let innhold;
+
+	var kommune_nr = document.getElementById("kom_nr").value; //henter ut verdien fra inputfeltet
+	for(var indeks in datasett.elementer){
+		let kom_nummer = datasett.elementer[indeks].kommunenummer;
+
+		//Sammenligner kommunenr.
+		if(kommune_nr == kom_nummer){
+			innhold = "Kommune: "+kom_nummer+ ", "+indeks +" - befolkning: "+ oversiktKommune(kommune_nr)+", sysselsetting: "
+			+ sisteSysselsetting() + ", Høyere utdanning: "+sisteHoyereUtdanning();
+		}
+	}
+	document.getElementById("detaljer").innerHTML = innhold;
+}
+
 
 
 
 //en test
 function test(){
-  console.log("test begynner")
-  console.log(utdannede);
-  console.log(befolkning);
-  console.log(sysselsatte);
-  console.log("test ferdig")
-  //document.getElementById("introduksjon").className = "show";
+	console.log("test begynner")
+	console.log(utdannede);
+	console.log(befolkning);
+	console.log(sysselsatte);
+	console.log("test ferdig")
+	//document.getElementById("introduksjon").className = "show";
 };
 setTimeout(test, 2000);
 
 //Vise og skjule divs
 
 function introfunk() {
-  document.getElementById("introduksjon").className = "show";
-  document.getElementById("oversikt").className = "hidden";
-  document.getElementById("detaljer").className = "hidden";
-  document.getElementById("sammenligning").className = "hidden";
+	document.getElementById("introduksjon").className = "show";
+	document.getElementById("oversikt").className = "hidden";
+	document.getElementById("detaljer").className = "hidden";
+	document.getElementById("sammenligning").className = "hidden";
 };
 
 
 function oversiktfunk() {
-  document.getElementById("introduksjon").className = "hidden";
-  document.getElementById("oversikt").className = "show";
-  document.getElementById("detaljer").className = "hidden";
-  document.getElementById("sammenligning").className = "hidden";
+	document.getElementById("introduksjon").className = "hidden";
+	document.getElementById("oversikt").className = "show";
+	document.getElementById("detaljer").className = "hidden";
+	document.getElementById("sammenligning").className = "hidden";
 	getOversikt();
 };
 
- function detaljfunk () {
-  document.getElementById("introduksjon").className = "hidden";
-  document.getElementById("oversikt").className = "hidden";
-  document.getElementById("detaljer").className = "show";
-  document.getElementById("sammenligning").className = "hidden";
+function detaljfunk () {
+	document.getElementById("introduksjon").className = "hidden";
+	document.getElementById("oversikt").className = "hidden";
+	document.getElementById("detaljer").className = "show";
+	document.getElementById("sammenligning").className = "hidden";
 };
 
-  function sammenfunk() {
-    document.getElementById("introduksjon").className = "hidden";
-    document.getElementById("oversikt").className = "hidden";
-    document.getElementById("detaljer").className = "hidden";
-    document.getElementById("sammenligning").className = "show";
+function sammenfunk() {
+	document.getElementById("introduksjon").className = "hidden";
+	document.getElementById("oversikt").className = "hidden";
+	document.getElementById("detaljer").className = "hidden";
+	document.getElementById("sammenligning").className = "show";
 };
 
 
@@ -201,45 +284,45 @@ function oversiktfunk() {
 /*
 function toggleDivs() {
 //Referanser til knapper
-  let introBtn = document.getElementById('intro_btn');
-  let overBtn = document.getElementById('oversikt_btn');
-  let detalBtn = document.getElementById('detalj_btn');
-  let sammenBtn = document.getElementById('sammenlign_btn');
+let introBtn = document.getElementById('intro_btn');
+let overBtn = document.getElementById('oversikt_btn');
+let detalBtn = document.getElementById('detalj_btn');
+let sammenBtn = document.getElementById('sammenlign_btn');
 
 //Referanser til divs
-  let introduksjon = document.getElementById('introduksjon');
-  let oversikt = document.getElementById('oversikt');
-  let detaljer = document.getElementById('detaljer');
-  let sammenligning = document.getElementById('sammenligning');
+let introduksjon = document.getElementById('introduksjon');
+let oversikt = document.getElementById('oversikt');
+let detaljer = document.getElementById('detaljer');
+let sammenligning = document.getElementById('sammenligning');
 
 
-  introBtn.onclick = function () {
-    introduksjon.className = "show";
-    oversikt.className = "hidden";
-    detaljer.className = "hidden";
-    sammenligning.className = "hidden";
-  };
+introBtn.onclick = function () {
+introduksjon.className = "show";
+oversikt.className = "hidden";
+detaljer.className = "hidden";
+sammenligning.className = "hidden";
+};
 
-  overBtn.onclick = function () {
-    introduksjon.className = "hidden";
-    oversikt.className = "show";
-    detaljer.className = "hidden";
-    sammenligning.className = "hidden";
-    getOversikt();
-  };
+overBtn.onclick = function () {
+introduksjon.className = "hidden";
+oversikt.className = "show";
+detaljer.className = "hidden";
+sammenligning.className = "hidden";
+getOversikt();
+};
 
-  detalBtn.onclick = function () {
-    introduksjon.className = "hidden";
-    oversikt.className = "hidden";
-    detaljer.className = "show";
-    sammenligning.className = "hidden";
-  };
+detalBtn.onclick = function () {
+introduksjon.className = "hidden";
+oversikt.className = "hidden";
+detaljer.className = "show";
+sammenligning.className = "hidden";
+};
 
-  sammenBtn.onclick = function () {
-    introduksjon.className = "hidden";
-    oversikt.className = "hidden";
-    detaljer.className = "hidden";
-    sammenligning.className = "show";
-  };
+sammenBtn.onclick = function () {
+introduksjon.className = "hidden";
+oversikt.className = "hidden";
+detaljer.className = "hidden";
+sammenligning.className = "show";
+};
 }
 */
